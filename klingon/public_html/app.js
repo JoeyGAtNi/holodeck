8d00abcdc79a8c7ef3b696756a3894d9b590ae4e
rev <rev@rev-VirtualBox.(none)>
2014-03-13 04:00:20 -0500

    updated the openaura

--- klingon/public_html/app.js
+++ klingon/public_html/app.js
@@ -14,6 +14,12 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 //app.use(express.static(__dirname + '/public'));
 
 
